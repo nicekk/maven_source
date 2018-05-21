f9dc6ca7f519ab368bd2624a447255f9e7031d84
nicekk <15268561861@163.com>
2018-05-21 20:48:16 +0800

    新增了IoUtils工具类

---
+++ src/main/java/com/dsj361/common/lang/IoUtils.java
@@ -0,0 +1,13 @@
+package com.dsj361.common.lang;
+
+import org.apache.commons.io.IOUtils;
+
+/**
+ * io工具类
+ * @Author wangkai
+ * @Date 2018-05-21 20:47:00
+ *
+ */
+public class IoUtils extends IOUtils {
+
+}
