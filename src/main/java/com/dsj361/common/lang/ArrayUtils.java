44006fcb8538449d50c152525a2cf72a57a5fac2
nicekk <15268561861@163.com>
2018-07-05 21:55:20 +0800

    新增了ArrayUtils.toList

--- src/main/java/com/dsj361/common/lang/ArrayUtils.java
+++ src/main/java/com/dsj361/common/lang/ArrayUtils.java
@@ -1,5 +1,8 @@
 package com.dsj361.common.lang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author wangkai
  * @date 2018/7/5 21:08
@@ -156,5 +159,172 @@ public class ArrayUtils extends org.apache.commons.lang3.ArrayUtils {
         }
 
         return (char[]) array.clone();
+    }
+
+    /**
+     * 将数组转换成<code>List</code>。
+     *
+     * <p>
+     * 如果输入数组为<code>null</code>，则返回<code>null</code>。
+     * </p>
+     *
+     * <p>
+     * 该方法返回的列表为指定数组的复本，而<code>java.util.Arrays.asList</code>
+     * 方法所返回的列表为指定数组的映像（固定长度）。
+     * </p>
+     *
+     * <p>
+     * 这个方法常被用于初始化，例如：
+     *
+     * <pre>
+     * List myList = ArrayUtil.toList(new String[] { &quot;aaa&quot;, &quot;bbb&quot;, &quot;ccc&quot; });
+     * List singleList = ArrayUtil.toList(&quot;hello&quot;); // 返回单个元素的列表[&quot;hello&quot;]
+     * </pre>
+     *
+     * </p>
+     *
+     * @param array
+     *            要转换的数组
+     *
+     * @return 被创建的list
+     */
+    public static List toList(Object array) {
+        return toList(array, null);
+    }
+
+    /**
+     * 将数组转换成<code>List</code>。
+     *
+     * <p>
+     * 如果输入数组为<code>null</code>，则返回<code>null</code>。
+     * </p>
+     *
+     * <p>
+     * 该方法返回的列表为指定数组的复本，而<code>java.util.Arrays.asList</code>
+     * 方法所返回的列表为指定数组的映像（固定长度）。
+     * </p>
+     *
+     * <p>
+     * 这个方法常被用于初始化，例如：
+     *
+     * <pre>
+     * List myList = ArrayUtil.toList(new String[] { &quot;aaa&quot;, &quot;bbb&quot;, &quot;ccc&quot; }, new ArrayList());
+     * List singleList = ArrayUtil.toList(&quot;hello&quot;, new ArrayList()); // 返回单个元素的列表[&quot;hello&quot;]
+     * </pre>
+     *
+     * </p>
+     *
+     * @param array
+     *            要转换的数组
+     * @param list
+     *            要填充的列表，如果是<code>null</code>，则创建之
+     *
+     * @return 被创建或填充的list
+     */
+    public static List toList(Object array, List list) {
+        if (array == null) {
+            return list;
+        }
+
+        // 非array，创建一个只有一个元素的列表
+        if (!array.getClass().isArray()) {
+            if (list == null) {
+                list = new ArrayList(1);
+            }
+
+            list.add(array);
+        } else if (array instanceof long[]) {
+            long[] longArray = (long[]) array;
+
+            if (list == null) {
+                list = new ArrayList(longArray.length);
+            }
+
+            for (int i = 0; i < longArray.length; i++) {
+                list.add(new Long(longArray[i]));
+            }
+        } else if (array instanceof int[]) {
+            int[] intArray = (int[]) array;
+
+            if (list == null) {
+                list = new ArrayList(intArray.length);
+            }
+
+            for (int i = 0; i < intArray.length; i++) {
+                list.add(new Integer(intArray[i]));
+            }
+        } else if (array instanceof short[]) {
+            short[] shortArray = (short[]) array;
+
+            if (list == null) {
+                list = new ArrayList(shortArray.length);
+            }
+
+            for (int i = 0; i < shortArray.length; i++) {
+                list.add(new Short(shortArray[i]));
+            }
+        } else if (array instanceof byte[]) {
+            byte[] byteArray = (byte[]) array;
+
+            if (list == null) {
+                list = new ArrayList(byteArray.length);
+            }
+
+            for (int i = 0; i < byteArray.length; i++) {
+                list.add(new Byte(byteArray[i]));
+            }
+        } else if (array instanceof double[]) {
+            double[] doubleArray = (double[]) array;
+
+            if (list == null) {
+                list = new ArrayList(doubleArray.length);
+            }
+
+            for (int i = 0; i < doubleArray.length; i++) {
+                list.add(new Double(doubleArray[i]));
+            }
+        } else if (array instanceof float[]) {
+            float[] floatArray = (float[]) array;
+
+            if (list == null) {
+                list = new ArrayList(floatArray.length);
+            }
+
+            for (int i = 0; i < floatArray.length; i++) {
+                list.add(new Float(floatArray[i]));
+            }
+        } else if (array instanceof boolean[]) {
+            boolean[] booleanArray = (boolean[]) array;
+
+            if (list == null) {
+                list = new ArrayList(booleanArray.length);
+            }
+
+            for (int i = 0; i < booleanArray.length; i++) {
+                list.add(booleanArray[i] ? Boolean.TRUE : Boolean.FALSE);
+            }
+        } else if (array instanceof char[]) {
+            char[] charArray = (char[]) array;
+
+            if (list == null) {
+                list = new ArrayList(charArray.length);
+            }
+
+            for (int i = 0; i < charArray.length; i++) {
+                list.add(new Character(charArray[i]));
+            }
+        } else {
+            Object[] objectArray = (Object[]) array;
+
+            if (list == null) {
+                list = new ArrayList(objectArray.length);
+            }
+
+            for (int i = 0; i < objectArray.length; i++) {
+                list.add(objectArray[i]);
+            }
+        }
+
+        return list;
     }
 }
